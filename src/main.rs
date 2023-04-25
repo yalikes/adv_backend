a81ac0feca3a316a14360b3c4927d4c98a06b596
paohui <2054453988@qq.com>
2023-04-25 12:17:15 +0800

    add testcookie

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use axum::{
     routing::{get, get_service, post},
     Router,
 };
-use hyper::http::HeaderValue;
+use std::{thread, time::Duration};
+use hyper::{http::HeaderValue, HeaderMap, header};
 use hyper::Method;
 use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
@@ -15,8 +16,8 @@ use tower_http::{
     services::ServeDir,
     trace::TraceLayer,
 };
-use tracing::info;
-use tracing_subscriber;
+use tracing::{info, debug};
+use tracing_subscriber::{self};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use dotenvy::dotenv;
@@ -32,6 +33,7 @@ async fn main() {
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
@@ -41,19 +43,34 @@ async fn main() {
             "/",
             get(|| async {
                 return "hello";
-            }),
-        )
+            }),)
+        .route("/testcookie", get(test_cookie))
         .layer(
             CorsLayer::new()
                 .allow_origin(AllowOrigin::list(
                     ["http://frontend.org", "http://frontend.org:5173"]
                         .map(|x| x.parse::<HeaderValue>().unwrap()),
                 ))
-                .allow_methods([Method::GET, Method::POST]),
-        );
+                .allow_methods([Method::GET, Method::POST])
+        )
+        .layer(TraceLayer::new_for_http());
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn test_cookie() -> impl IntoResponse{
+    // CORS cookie 不好搞, 用JSON吧
+    let mut header = HeaderMap::new();
+    let header_val = HeaderValue::from_str("text/html; charset=utf-8").unwrap();
+    debug!("{:?}",header_val);
+    header.insert(
+        header::CONTENT_TYPE,
+        header_val,
+    );
+    header.insert(header::SET_COOKIE, HeaderValue::from_str("A=12").unwrap());
+    thread::sleep(Duration::from_secs(5));
+    (StatusCode::OK, header, "hello world!".to_owned())
 }
