b340e4d1f4b84f4a44e0c18880b0341e6464cb73
paohui <2054453988@qq.com>
2023-04-27 00:02:32 +0800

    I decide to use json, and not cookie

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@ use axum::{
     http::StatusCode,
     response::IntoResponse,
     routing::{get, get_service, post},
-    Router,
+    Router, Json,
 };
-use std::{thread, time::Duration};
-use hyper::{http::HeaderValue, HeaderMap, header};
+use dotenvy::dotenv;
 use hyper::Method;
+use hyper::{header, http::HeaderValue, HeaderMap};
+use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
 use std::{collections::HashMap, env};
+use std::{thread, time::Duration};
 use tower_http::cors::{AllowOrigin, Cors};
 use tower_http::{
     cors::{Any, CorsLayer},
     services::ServeDir,
     trace::TraceLayer,
 };
-use tracing::{info, debug};
+use tracing::{debug, info};
 use tracing_subscriber::{self};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
@@ -37,21 +37,23 @@ async fn main() {
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
-        .await.expect("failed to connect database");
+        .await
+        .expect("failed to connect database");
     let app = Router::new()
         .route(
             "/",
             get(|| async {
                 return "hello";
-            }),)
-        .route("/testcookie", get(test_cookie))
+            }),
+        )
+        .route("/testapi", get(test_api))
         .layer(
             CorsLayer::new()
                 .allow_origin(AllowOrigin::list(
                     ["http://frontend.org", "http://frontend.org:5173"]
                         .map(|x| x.parse::<HeaderValue>().unwrap()),
                 ))
-                .allow_methods([Method::GET, Method::POST])
+                .allow_methods([Method::GET, Method::POST]),
         )
         .layer(TraceLayer::new_for_http());
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
@@ -61,16 +63,15 @@ async fn main() {
         .unwrap();
 }
 
-async fn test_cookie() -> impl IntoResponse{
+#[derive(Debug, Serialize, Deserialize)]
+struct Session {
+    session_id: String,
+}
+
+async fn test_api() -> impl IntoResponse {
+    let session_id = Session {
+        session_id: "1".to_owned(),
+    };
     // CORS cookie 不好搞, 用JSON吧
-    let mut header = HeaderMap::new();
-    let header_val = HeaderValue::from_str("text/html; charset=utf-8").unwrap();
-    debug!("{:?}",header_val);
-    header.insert(
-        header::CONTENT_TYPE,
-        header_val,
-    );
-    header.insert(header::SET_COOKIE, HeaderValue::from_str("A=12").unwrap());
-    thread::sleep(Duration::from_secs(5));
-    (StatusCode::OK, header, "hello world!".to_owned())
+    Json::from(session_id)
 }
--- table_definition/create_tables.sql
+++ table_definition/create_tables.sql
@@ -1,3 +1,25 @@
-CREATE TABLE message(
-    
+CREATE TABLE private_message(
+    message_id bigserial primary key,
+    message varchar(4096),
+    message_from REFERENCES user,
+    message_to REFERENCES user,
+    created_at timestamp
+);
+CREATE TABLE group_message(
+    group_message_id bigserial primary key,
+    group_id REFERENCES group
+    group_message varchar(4096),
+    message_from REFERENCES user,
+    group_admin REFERENCES user,
+    admin_list bigint[],
+);
+
+CREATE TRIGGER check
+
+CREATE TABLE group(
+
+);
+
+CREATE TABLE user(
+
 );
